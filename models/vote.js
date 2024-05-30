646ece90114b957da4508c92ce82c0c7c7e234c0
Daniel Mathews <daniel.g.mathews@gmail.com>
2024-05-30 13:14:30 -0400

    add associations to vote and profile models

--- models/vote.js
+++ models/vote.js
@@ -11,6 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Vote.belongsTo(models.Profile, { foreignKey: 'voterId'})
+      Vote.belongsTo(models.Profile, { foreignKey: 'profileId'})
     }
   }
   Vote.init({
